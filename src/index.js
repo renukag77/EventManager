fc46ef5a8f3edbf6d9c2c8957a0b9c283b9accb4
renukag77 <147975015+renukag77@users.noreply.github.com>
2024-09-29 15:09:37 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.js';  // Add .js extension
+import reportWebVitals from './reportWebVitals.js';  // Add .js extension
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
